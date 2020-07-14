0ccc392334084e23cd7b138a31b5ff081ee3f705
Marcos Cabral <42642744+Marcos-Cabral@users.noreply.github.com>
2020-07-14 20:42:31 -0300

    final

--- tp/bd_php.sql
+++ tp/bd_php.sql
@@ -1,26 +1,11 @@
 create database infonete;
 use infonete;
 
-insert into rol values(1,"usuario");
-insert into rol values(2,"admin");
-insert into rol values(3,"contenidista");
-
-insert into usuariosuscribeejemplar values('2020-06-12',3,2,1); 
-insert into usuariosuscribeejemplar values('2020-06-13',3,1,1); 
-insert into usuariosuscribeejemplar values('2020-06-12',3,3,1); 
-insert into usuariosuscribeejemplar values('2020-06-13',3,1,1); 
-insert into usuariocompraedicion values(3,2,'2020-07-12');
-
 create table rol(
 	id int not null auto_increment,
     nombre varchar(25),
     primary key(id)
 );
-
-insert into usuario values('1',"Facundo D' Aranno","facu","1234","19990101","facu@gmail.com",1599030188,"san justo",2);
-insert into usuario values('2',"Alejandro Rusticcini","ale","1234","19990101","ale@gmail.com",1501014558,"san justo",3);
-insert into usuario values('3',"Omar Sosa","omar","1234","19990101","omar@gmail.com",1597840101,"san justo",3);
-insert into categoria values('1','diario'),('2','revista');
 
 create table usuario(
 	id integer not null auto_increment,
@@ -134,26 +119,8 @@ create table usuarioLeeNoticia(
     foreign key(id_noticia) references noticia(id)
 );
 
-/*TODO ESTO ES PARA LOS GRAFICOS*/
-
-insert into edicion values
-('2', 'Otoñal', '2', '1', true, 3.00),
-(3, 'Oscars', 2020, 2, true, 50.00);
-
-insert into usuariocompraedicion values
-(1, 3, '2020-06-20'),
-(3, 1, '2020-08-20'),
-(3, 3, '2020-07-20'),
-(3, 2, '2020-02-20');
-
-insert into usuariocompraedicion values
-(2, 1, '2020-08-20');
-
-insert into ejemplar values
-(4, 'TKM', 2, true, 50.0);
-
-insert into usuariosuscribeejemplar values
-('2020-06-20', 3, 1, true),
-('2020-06-21', 3, 2, true),
-('2020-06-20', 2, 3, true);
+insert into categoria values('1','diario'),('2','revista');
+insert into rol values(1,"usuario");
+insert into rol values(2,"admin");
+insert into rol values(3,"contenidista");
 
